e4458de3cdc3cc0051f2409f4c235f6b7e999141
dexter.chen <2406010@iisigroup.com>
2024-09-11 10:47:02 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Welcome to my Vue page!</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script lang="ts">
+// import Tec1004View from '@/components/tec/tec1004/view/index.vue';
+
+export default {
+  // name: 'tec0404AView',
+  // components: {
+  //   Tec1004View,
+  // },
+  // props: {
+  //   fileProps: {
+  //     type: Object,
+  //     required: true,
+  //   },
+  // },
+
+  setup(props) {
+    const message = 'Hello, world!';
+    return {
+      message
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
